df889552c1e1bd9b65f9e8b5f0c40687ad60a92b
Zan-Jun, Wang <twzjwang@gmail.com>
2018-12-18 15:38:46 +0800

    Add config

---
+++ Config.py
@@ -0,0 +1,18 @@
+Config = dict(
+    providerURL = "",
+    blindPublicKeyExponent = ,
+    blindModulus = ,
+    contractAddress = "",
+    organizerAddress = "",
+    voterAddress = "",
+)
+
+voterConfig = dict(
+    voterChoice = 1,
+    voterPrivateKey = "",
+)
+
+organizerConfig = dict(
+    blindPrivateKeyExponent = ,
+    voterPrivateKey = "",
+)
