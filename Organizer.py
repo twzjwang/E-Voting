b18a43c59db13d7323b4b57283065bd81008b7d2
Zan-Jun, Wang <twzjwang@gmail.com>
2018-12-18 16:09:04 +0800

    Add ContractFactoryClass

--- Organizer.py
+++ Organizer.py
@@ -1,4 +1,5 @@
 from web3 import Web3
+from web3.contract import ConciseContract
 from eth_account.messages import defunct_hash_message
 import Ballot.ballot as Ballot
 import Blind_Signature.RSA.blind as Blind
@@ -6,4 +7,4 @@ import Config
 
 if __name__ == '__main__':
     web3 = Web3(Web3.HTTPProvider(Config.providerURL))
-    contractInstance = web3.eth.contract(address=web3.toChecksumAddress(Config.contractAddress), abi=Config.contractABI)
+    contractInstance = web3.eth.contract(address=web3.toChecksumAddress(Config.contractAddress), abi=Config.contractABI, ContractFactoryClass=ConciseContract)
--- Voter.py
+++ Voter.py
@@ -1,4 +1,5 @@
 from web3 import Web3
+from web3.contract import ConciseContract
 from eth_account.messages import defunct_hash_message
 import Ballot.ballot as Ballot
 import Blind_Signature.RSA.blind as Blind
@@ -6,7 +7,7 @@ import Config
 
 if __name__ == '__main__':
     web3 = Web3(Web3.HTTPProvider(Config.providerURL))
-    contractInstance = web3.eth.contract(address=web3.toChecksumAddress(Config.contractAddress), abi=Config.contractABI)
+    contractInstance = web3.eth.contract(address=web3.toChecksumAddress(Config.contractAddress), abi=Config.contractABI, ContractFactoryClass=ConciseContract)
 
     print('***Alice prepares ballot***')
 
