bbc18146249ea4c26a967f565ae6bd9126a481eb
Zan-Jun, Wang <twzjwang@gmail.com>
2019-01-03 18:29:19 +0800

    Add behaviors of voters and organizer

--- Organizer.py
+++ Organizer.py
@@ -1,10 +1,38 @@
 from web3 import Web3
-from web3.contract import ConciseContract
 from eth_account.messages import defunct_hash_message
 import Ballot.ballot as Ballot
 import Blind_Signature.RSA.blind as Blind
+from configparser import ConfigParser
+import time
 import Config
 
+config = ConfigParser()
+config.read('Config.ini')
+
+web3 = Web3(Web3.HTTPProvider(Config.providerURL))
+account = web3.eth.account.privateKeyToAccount(Config.organizerConfig['organizerPrivateKey'])
+contractInstance = web3.eth.contract(address=web3.toChecksumAddress(config.get('Global', 'contractaddress')), abi=config.get('Global', 'contractABI'))
+blindPublicKey = int(contractInstance.functions.blindPublicKey().call())
+blindModulus = int(contractInstance.functions.blindModulus().call())
+
+def handle_event(event):
+    print('\n***Bob receives the blind message and signs it***')
+    print(event)
+    signedBlindedMsg = Blind.signature(event['args']['msg'], Blind.Key(Config.organizerConfig['blindPrivateKeyExponent'], blindModulus))
+    print('\n***Bob sends the signed, blinded message to the smart contract***')
+    print(event['args']['msg'], str(signedBlindedMsg))
+    web3.eth.defaultAccount = account.address
+    contractInstance.functions.sendSignedBlindMessage(event['args']['msg'], str(signedBlindedMsg)).transact()
+
+def log_loop(event_filter, poll_interval):
+    while True:
+        for event in event_filter.get_new_entries():
+            handle_event(event)
+        time.sleep(poll_interval)
+
 if __name__ == '__main__':
-    web3 = Web3(Web3.HTTPProvider(Config.providerURL))
-    contractInstance = web3.eth.contract(address=web3.toChecksumAddress(Config.contractAddress), abi=Config.contractABI, ContractFactoryClass=ConciseContract)
+    print('\n***Bob listens to the smart contract***')
+    block_filter = contractInstance.events.newBlindMessage.createFilter(fromBlock='0x0')
+    log_loop(block_filter, 1)
+
+
--- Voter.py
+++ Voter.py
@@ -3,20 +3,55 @@ from web3.contract import ConciseContract
 from eth_account.messages import defunct_hash_message
 import Ballot.ballot as Ballot
 import Blind_Signature.RSA.blind as Blind
+from configparser import ConfigParser
+import time
 import Config
 
 if __name__ == '__main__':
+
+    config = ConfigParser()
+    config.read('Config.ini')
+
     web3 = Web3(Web3.HTTPProvider(Config.providerURL))
-    contractInstance = web3.eth.contract(address=web3.toChecksumAddress(Config.contractAddress), abi=Config.contractABI, ContractFactoryClass=ConciseContract)
+    account1 = web3.eth.account.privateKeyToAccount(Config.voterConfig['voterPrivateKey1'])
+    account2 = web3.eth.account.privateKeyToAccount(Config.voterConfig['voterPrivateKey2'])
+    contractInstance = web3.eth.contract(address=web3.toChecksumAddress(config.get('Global', 'contractaddress')), abi=config.get('Global', 'contractABI'))
+    blindPublicKey = int(contractInstance.functions.blindPublicKey().call())
+    blindModulus = int(contractInstance.functions.blindModulus().call())
 
     print('***Alice prepares ballot***')
-
     voteString = Ballot.generateVoteString(Config.voterConfig['voterChoice'])
     print('Vote String', voteString)
-
     hashV = Ballot.hashVoteString(voteString)
     print('Hashed Vote String', hashV)
 
     print('\n***Alice blinds the message***')
-    publicKey = contractInstance.blindPublicKey()
-    r = Blind.blind(int(hashV, 16), Blind.Key(int(publicKey[0]), int(publicKey[1])))
+    r, blindedMessage = Blind.blind(hashV, Blind.Key(blindPublicKey, blindModulus))
+    print(blindedMessage)
+
+    print('\n***Alice sends the blinded message to the smart contract***')
+    web3.eth.defaultAccount = account1.address
+    contractInstance.functions.sendBlindMessage(str(blindedMessage)).transact()
+
+    print('\n***Alice listens to the smart contract to get signed blinded message***')
+    event_filter = contractInstance.events.newSignedBlindMessage.createFilter(fromBlock='latest')
+    isReceive = 0
+    signedBlindedMsg = ''
+    while not isReceive:
+        for event in event_filter.get_new_entries():
+            print(event)
+            if event['args']['unsignedMsg'] == blindedMessage:
+                signedBlindedMsg = event['args']['signedMsg']
+                isReceive = 1
+                break
+        time.sleep(1)
+    
+    print('\n***Alice recieves the signed message and unblinds it***')
+    signedMsg = Blind.unblind(signedBlindedMsg, r, Blind.Key(blindPublicKey, blindModulus))
+
+    print('\n***Alice send the ballot and the signed message to the smart contrac***')
+    web3.eth.defaultAccount = account2.address
+    print(str(voteString), str(signedMsg))
+    contractInstance.functions.sendBallot(str(voteString), str(signedMsg)).transact()
+
+    print('\n***Alice has voted her ballot***')
